469150f93b87ba99f4df7befa511bb2b22d56cc4
AndreaVillegasGarcia <a.villegas@alumnos.upm.es>
2020-04-30 22:32:12 +0200

    Recibiendo datos y mostrando en chart de prueba

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -1,67 +1,40 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-undef */
 $(document).ready(() => {
-  // if deployed to a site supporting SSL, use wss://
+
   const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
   const webSocket = new WebSocket(protocol + location.host);
-
-  // A class for holding the last N points of telemetry for a device
-  class DeviceData {
-    constructor() {
-      this.deviceId = deviceId;
-      this.maxLen = 50;
-      this.timeData = new Array(this.maxLen);
-      this.temperatureData = new Array(this.maxLen);
-      this.humidityData = new Array(this.maxLen);
-    }
-
-    addData(time, temperature, humidity) {
-      this.timeData.push(time);
-      this.temperatureData.push(temperature);
-      this.humidityData.push(humidity || null);
-
-      if (this.timeData.length > this.maxLen) {
-        this.timeData.shift();
-        this.temperatureData.shift();
-        this.humidityData.shift();
-      }
+  
+  var maxLen = 50;
+  
+  function addData(device, tempIn) {
+    console.log("Dev: " + device + " Temp: " + tempIn)
+    chartData.datasets[device].data.push(tempIn)
+    chartData.labels.push(new Date().getTime("HH:mm:ss"));
+    if (chartData.datasets[device].data.length > maxLen) {
+      chartData.datasets[device].data.shift();
     }
   }
 
-  // All the devices in the list (those that have been sending telemetry)
-  class TrackedDevices {
-    constructor() {
-      this.devices = [];
+  function format(message){
+    console.log(message.IotData.avtemperatureIn)
+    let tempIn = message.IotData.avtemperatureIn;
+    let device = -1;
+    if (message.DeviceId == "MyDevice1"){
+      device = 0
+    } else if (message.DeviceId == "MyDevice2"){
+      device = 1
+    } else if (message.DeviceId == "MyDevice3"){
+      device = 2
     }
-
-    // Find a device based on its Id
-    findDevice(deviceId) {
-      for (let i = 0; i < this.devices.length; ++i) {
-        if (this.devices[i].deviceId === deviceId) {
-          return this.devices[i];
-        }
-      }
-
-      return undefined;
-    }
-
-    getDevicesCount() {
-      return this.devices.length;
-    }
-
-    formatData(){
-
-    }
+    addData(device, tempIn)
+    myLineChart.update();
   }
 
-  const trackedDevices = new TrackedDevices();
-
-  // Define the chart axes
   const chartData = {
+    labels: [],
     datasets: [
       {
-        fill: false,
+        data: [],
+        fill: true,
         label: 'Temperature',
         yAxisID: 'Temperature',
         borderColor: 'rgba(255, 204, 0, 1)',
@@ -69,11 +42,12 @@ $(document).ready(() => {
         backgroundColor: 'rgba(255, 204, 0, 0.4)',
         pointHoverBackgroundColor: 'rgba(255, 204, 0, 1)',
         pointHoverBorderColor: 'rgba(255, 204, 0, 1)',
-        spanGaps: true,
+        spanGaps: false,
       },
       {
         fill: false,
         label: 'Humidity',
+        data: [],
         yAxisID: 'Humidity',
         borderColor: 'rgba(24, 120, 240, 1)',
         pointBoarderColor: 'rgba(24, 120, 240, 1)',
@@ -87,6 +61,7 @@ $(document).ready(() => {
 
   const chartOptions = {
     scales: {
+      xAxes: [{}],
       yAxes: [{
         id: 'Temperature',
         type: 'linear',
@@ -108,7 +83,6 @@ $(document).ready(() => {
     }
   };
 
-  // Get the context of the canvas element we want to select
   const ctx = document.getElementById('iotChart').getContext('2d');
   const myLineChart = new Chart(
     ctx,
@@ -118,63 +92,12 @@ $(document).ready(() => {
       options: chartOptions,
     });
 
-  // Manage a list of devices in the UI, and update which device data the chart is showing
-  // based on selection
-  let needsAutoSelect = true;
-  const deviceCount = document.getElementById('deviceCount');
-  const listOfDevices = document.getElementById('listOfDevices');
-  function OnSelectionChange() {
-    const device = trackedDevices.findDevice(listOfDevices[listOfDevices.selectedIndex].text);
-    chartData.labels = device.timeData;
-    chartData.datasets[0].data = device.temperatureData;
-    chartData.datasets[1].data = device.humidityData;
-    myLineChart.update();
-  }
-  listOfDevices.addEventListener('change', OnSelectionChange, false);
-
-  // When a web socket message arrives:
-  // 1. Unpack it
-  // 2. Validate it has date/time and temperature
-  // 3. Find or create a cached device to hold the telemetry data
-  // 4. Append the telemetry data
-  // 5. Update the chart UI
   webSocket.onmessage = function onMessage(message) {
     try {
       const messageData = JSON.parse(message.data);
       console.log(messageData);
+      format(messageData);
 
-      // time and either temperature or humidity are required
-      if (!messageData.MessageDate || (!messageData.IotData.temperature && !messageData.IotData.humidity)) {
-        return;
-      }
-
-      // find or add device to list of tracked devices
-      const existingDeviceData = trackedDevices.findDevice(messageData.DeviceId);
-
-      if (existingDeviceData) {
-        existingDeviceData.addData(messageData.MessageDate, messageData.IotData.temperature, messageData.IotData.humidity);
-      } else {
-        const newDeviceData = new DeviceData(messageData.DeviceId);
-        trackedDevices.devices.push(newDeviceData);
-        const numDevices = trackedDevices.getDevicesCount();
-        deviceCount.innerText = numDevices === 1 ? `${numDevices} device` : `${numDevices} devices`;
-        newDeviceData.addData(messageData.MessageDate, messageData.IotData.temperature, messageData.IotData.humidity);
-
-        // add device to the UI list
-        const node = document.createElement('option');
-        const nodeText = document.createTextNode(messageData.DeviceId);
-        node.appendChild(nodeText);
-        listOfDevices.appendChild(node);
-
-        // if this is the first device being discovered, auto-select it
-        if (needsAutoSelect) {
-          needsAutoSelect = false;
-          listOfDevices.selectedIndex = 0;
-          OnSelectionChange();
-        }
-      }
-
-      myLineChart.update();
     } catch (err) {
       console.error(err);
     }
